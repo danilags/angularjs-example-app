6a902fbaa7268882134684d8fb18d0dec8b7869a
Daniel Sidabutar <aguspermadi94@gmail.com>
2017-12-28 02:16:35 +0700

    Make hero detail component

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Initial App, Teman Diabetes';
+  title = 'Initial App, Avengers!';
   content = 'Lorem Ipsum Dolor Sit Amet';
 }
